ff5dfba0ef7d79c9eb61b03d269d5b70317b656e
Nicolás Snider <nicolas.snider@outlook.com>
2025-05-17 00:14:15 -0300

    Add IUnitOfWork and IUserRepository interfaces
    Introduces two new interfaces: IUnitOfWork for managing
    repository access and saving changes asynchronously,
    and IUserRepository for CRUD operations on user entities.

---
+++ src/TKDHubAPI.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace TKDHubAPI.Domain.Repositories;
+public interface IUnitOfWork
+{
+    IUserRepository Users { get; }
+    // Add other repositories as needed, e.g. IRankRepository Ranks { get; }
+    Task<int> SaveChangesAsync();
+}
---
+++ src/TKDHubAPI.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using TKDHubAPI.Domain.Entities;
+
+namespace TKDHubAPI.Domain.Repositories;
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(int id);
+    Task<IEnumerable<User>> GetAllAsync();
+    Task AddAsync(User user);
+    void Update(User user);
+    void Remove(User user);
+}
