a6d41954746b2b6e9c361724d021ee8a20885b8b
Nicolás Snider <nicolas.snider@outlook.com>
2025-05-16 23:51:06 -0300

    Add JWT authentication and Swagger support
    Introduce a new `DependencyInjection` class with an `AddWebAPIServices` method to configure web API services, including controllers, endpoints, and JWT authentication. Update `Program.cs` to utilize this method for service registration, enhancing organization and separation of concerns. Also, add necessary package references for `Microsoft.AspNetCore.Authentication.JwtBearer` and `Swashbuckle.AspNetCore` in the project file.

---
+++ src/TKDHubAPI.WebAPI/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace TKDHubAPI.WebAPI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebAPIServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        // Authentication
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => { /* JWT options */ });
+        return services;
+    }
+}
