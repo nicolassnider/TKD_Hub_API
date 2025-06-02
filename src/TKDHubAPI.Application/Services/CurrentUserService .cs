e81ffdb917bfff65960b3ac2ac7804ee332f5d67
Nicolás Snider <nicolas.snider@outlook.com>
2025-06-01 23:43:21 -0300

    add using http to CurrentUserService

--- src/TKDHubAPI.Application/Services/CurrentUserService .cs
+++ src/TKDHubAPI.Application/Services/CurrentUserService .cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace TKDHubAPI.Application.Services;
 public class CurrentUserService : ICurrentUserService
