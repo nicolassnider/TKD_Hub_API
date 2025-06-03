51bb6f5f06a986de5f6b31e4eb712316929d2389
Nicolás Snider <nicolas.snider@outlook.com>
2025-06-02 22:46:18 -0300

    Add RanksController and refactor StudentsController
    - Introduced `RanksController` for rank management with methods to retrieve all ranks and a specific rank by ID using `IRankService`.
    - Replaced `IUserService` with `IStudentService` in `StudentsController`, updating constructor and logic for student creation and retrieval.
    - Added new methods in `StudentsController` to get all students and retrieve students by specific "dojaang" ID.

---
+++ src/TKDHubAPI.WebAPI/Controllers/RanksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TKDHubAPI.Application.Interfaces;
+
+namespace TKDHubAPI.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RanksController : ControllerBase
+{
+    private readonly IRankService _rankService;
+
+    public RanksController(IRankService rankService)
+    {
+        _rankService = rankService;
+    }
+
+    // GET: api/Ranks
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var ranks = await _rankService.GetAllAsync();
+        return Ok(ranks);
+    }
+
+    // GET: api/Ranks/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var rank = await _rankService.GetByIdAsync(id);
+        if (rank == null)
+            return NotFound();
+        return Ok(rank);
+    }
+}
