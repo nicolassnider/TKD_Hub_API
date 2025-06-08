753bc7a80520d1cde059505244493ae2735388bd
Nicolás Snider <nicolas.snider@outlook.com>
2025-06-08 12:46:37 -0300

    update sql statements for testing purposes

--- sql.data/Students.sql
+++ sql.data/Students.sql
@@ -1,3 +1,4 @@
+-- add 23 students
 USE [TKDHubDb]
 GO
 
@@ -26,4 +27,35 @@ VALUES
 ('Martina', 'Cruz', 'martina.cruz@example.com', '', DATEADD(YEAR, -ABS(CHECKSUM(NEWID()) % 9) - 7, GETDATE()), 'female', '+54 9 11 1234-5721', 1, ABS(CHECKSUM(NEWID()) % 9) + 1, DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()), CAST(CAST(ABS(CHECKSUM(NEWID()) % 2) AS INT) AS BIT), DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()), DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE())),
 ('Pablo', 'García', 'pablo.garcia@example.com', '', DATEADD(YEAR, -ABS(CHECKSUM(NEWID()) % 9) - 7, GETDATE()), 'male', '+54 9 11 1234-5722', 1, ABS(CHECKSUM(NEWID()) % 9) + 1, DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()), CAST(CAST(ABS(CHECKSUM(NEWID()) % 2) AS INT) AS BIT), DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()), DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE())),
 ('Agustín', 'Hernández', 'agustin.hernandez@example.com', '', DATEADD(YEAR, -ABS(CHECKSUM(NEWID()) % 9) - 7, GETDATE()), 'male', '+54 9 11 1234-5723', 1, ABS(CHECKSUM(NEWID()) % 9) + 1, DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()), CAST(CAST(ABS(CHECKSUM(NEWID()) % 2) AS INT) AS BIT), DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()), DATEADD(YEAR, ABS(CHECKSUM(NEWID()) % 5) + 2020, GETDATE()));
+GO
+
+
+-- manage roles
+
+USE [TKDHubDb]
+GO
+
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (32, 3, 30);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (33, 2, 31);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (34, 2, 32);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (35, 2, 33);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (36, 2, 34);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (37, 2, 35);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (38, 2, 36);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (39, 2, 37);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (40, 2, 38);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (41, 2, 39);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (42, 2, 40);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (43, 2, 41);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (44, 2, 42);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (45, 2, 43);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (46, 2, 44);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (47, 2, 45);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (48, 2, 46);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (49, 2, 47);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (50, 2, 48);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (51, 2, 49);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (52, 2, 50);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (53, 2, 51);
+INSERT INTO [dbo].[UserUserRoles] ([UserId], [UserRoleId], [Id]) VALUES (54, 2, 52);
 GO
